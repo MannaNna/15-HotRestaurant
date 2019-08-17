0655050e6472f6d61f889902fc57b9a00a67e38e
Matthew <mattg1827@gmail.com>
2019-08-17 12:31:22 -0700

    Server update

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var path = require("path");
 
 // Sets up the Express App
 // =============================================================
+
 var app = express();
 var PORT = 3000;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Loading the HTML pages 
+// =============================================================
 
 app.get("/", function(req, res) {
 res.sendFile(path.join(__dirname, "home.html"));
@@ -21,8 +24,27 @@ app.get("/reserve", function(req, res) {
 res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+// API pages 
+// =============================================================
+
+app.get("/api/tables", function(req, res) {
+return res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+return res.json(waitlist);
+});
+
+// The Data Objects
+// =============================================================
+
+var tables = [];
+var waitlist = [];
+
+// Begin to listen to the PORT to make the connection for our server
+// =============================================================
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+console.log("App listening on PORT " + PORT);
+});
   
