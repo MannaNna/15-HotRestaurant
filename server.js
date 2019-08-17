e4f1a8102d2d43e377512692b1bf504ca7de969c
Matthew <mattg1827@gmail.com>
2019-08-17 12:07:17 -0700

    Server.js work

--- server.js
+++ server.js
@@ -10,9 +10,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+res.sendFile(path.join(__dirname, "home.html"));
+});
+  
+app.get("/tables", function(req, res) {
+res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
   
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
-  });
